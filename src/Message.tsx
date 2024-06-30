62b650056030cdbed617119b273d01aa5e91d0f4
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-06-30 15:10:25 +0300

    First projo using react

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// creating a react component  //pascalcasing 
+function Message() {
+    //javascript XML
+    const name = 'Daniel';
+    if (name )
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World </h1>
+}
+
+export default Message;
