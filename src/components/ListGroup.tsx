23e0fa5abf8a8fc8951e551b0be7465c56d0898b
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-06-30 18:35:05 +0300

    First react projo

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
+
+
 function ListGroup() {
+
+  const items = [
+    'Nairobi',
+    'Naivasha',
+    'Naks'
+  ];
+ 
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-      <li className="list-group-item">And a sixth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+         {items.map((item) => (<li key= {item}>{item}</li>))}
+      </ul>
+    </>
   );
 }
 
